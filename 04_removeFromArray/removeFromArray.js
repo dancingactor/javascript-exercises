f0a92f0b81b0755c0eb3d68b7dd963f4f6d67f8a
dancingactor <s990346@gmail.com>
2024-07-21 12:32:12 +0800

    Assignment 4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...removedItem) {
+    
+    //version 1, use the forEach() method
+    /*
+    const returnArray = [];
+    array.forEach(
+        item => { if(!removedItem.includes(item)) {
+            returnArray.push(item);
+        }        }
+    )
+    return returnArray;
+    */
+    
+    
+    //version 2, more concise way
+    return array.filter(item => !removedItem.includes(item))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
