c3f510f56dffc279e55659ecf51faa8b21e44446
dancingactor <s990346@gmail.com>
2024-08-07 16:37:32 +0800

    Assignment 8 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(...numbers) {
+	return numbers.reduce((acc, cur) => acc + cur);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((acc, cur) => acc + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, cur) => acc * cur, 1);
 };
 
-const power = function() {
-	
+const power = function(base, expo) {
+  return Math.pow(base, expo);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1;
+  for (let i = num; i > 1; i--) {
+    product *= i;
+  }
+  return product;
+
 };
 
 // Do not edit below this line
