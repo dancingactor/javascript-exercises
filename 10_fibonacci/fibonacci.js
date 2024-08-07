5178d790adfba4ff98b46d2e17e36e5111f209af
dancingactor <s990346@gmail.com>
2024-08-07 23:22:18 +0800

    Assignment 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(numth) {
+    let array = [0, 1];
+    numth = parseInt(numth);
+    if (numth < 0) {
+        return "OOPS";
+    }
+    if (numth < 2) {
+        return array[numth];
+    }
+
+    for (i = 2; i <= numth; i++) {
+        array.push(array[i-2] + array[i-1]);
+    }
+    return array[numth];
 
 };
 
