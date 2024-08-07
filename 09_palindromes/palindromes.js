de1586126315dd1d889e97499cac64469bd98e05
dancingactor <s990346@gmail.com>
2024-08-07 17:43:51 +0800

    Assignment 9 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    let alphabetlist = "abcdefghijklmnopqrstuvwxyz0123456789";
+    let cleanedString = string
+        .toLowerCase()
+        .split("")
+        .filter((item) => alphabetlist.split("").includes(item))
+        .join("");
+    let reversedString = cleanedString.split("").reverse().join("");
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
