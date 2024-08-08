bf72bcdbb83f98cc2647f10e6483f0cb05ed0105
dancingactor <s990346@gmail.com>
2024-08-08 16:30:16 +0800

    Assignment 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
 
+function getAge(person) {
+  if (!person.yearOfDeath) {
+    person.yearOfDeath = new Date().getFullYear();
+  }
+  return person.yearOfDeath - person.yearOfBirth;
+}
+
+const findTheOldest = function(people) {
+  const oldestPerson = people.reduce((oldest, currentPerson) => {
+    return getAge(currentPerson) > getAge(oldest) ? currentPerson : oldest;
+  });
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
